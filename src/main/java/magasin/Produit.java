b0ab970ea0a7cd03a4f6dae08407d0df15e31336
Mikael Valot <mikael.valot@efrei.fr>
2025-10-02 19:06:46 +0200

    initial commit

---
+++ src/main/java/magasin/Produit.java
@@ -0,0 +1,11 @@
+package magasin;
+
+public class Produit {
+    private int id;
+    private String nom;
+    private double prix;
+
+    protected void setPrix(double prix) {
+        this.prix = prix;
+    }
+}
